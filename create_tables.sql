605297b609ccc091dc3f7b219997b70a89bc457b
Lars Jensen <lars.c.jensen@gmail.com>
2024-04-07 11:47:01 +0200

    Change order to make atonomous run

--- create_tables.sql
+++ create_tables.sql
@@ -1,56 +1,12 @@
 CREATE DATABASE green_rental COLLATE latin1_swedish_ci;
+USE green_rental;
 
--- MySQL ignores capital letters, so keeping them lower case
-CREATE TABLE greenrental.station (
-  stationName VARCHAR(50) NOT NULL PRIMARY KEY,
-  streetName VARCHAR(50) NOT NULL,
-  zipCode VARCHAR(10) NULL,
-  city VARCHAR(50) NULL,
-  country VARCHAR(50) NULL  
-);
-
-CREATE TABLE greenrental.vehicle_category(
-	vehicleCategoryId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL,
-    propellant VARCHAR(50) NOT NULL,
-    automatic BOOL,
-    numberOfSeats INT NOT NULL, -- Do we want default value instead?
-    misc VARCHAR(50),
-    costPerKm FLOAT NOT NULL,
-    price FLOAT NOT NULL
-);
-
-CREATE TABLE greenrental.vehicle (
-  registrationNumber VARCHAR(6) NOT NULL PRIMARY KEY,
-  stationName VARCHAR(50) NOT NULL,
-  vehicleCategoryId INT NOT NULL,
-  model VARCHAR(32) NOT NULL,
-  FOREIGN KEY (stationName) REFERENCES station(stationName) 
-  ON DELETE NO ACTION -- Create test for this
-  ON UPDATE NO ACTION,
-  FOREIGN KEY (vehicleCategoryId) REFERENCES vehicle_category(vehicleCategoryId)
-  ON DELETE NO ACTION -- Create test for this
-  ON UPDATE NO ACTION
-);
-
-
-# test
-CREATE DATABASE green_rental;
-
-CREATE TABLE staff (
-    staffID INT PRIMARY KEY,
-    stationId INT,
-    preName VARCHAR(50),
-    surName VARCHAR(50),
+CREATE TABLE station (
+    stationName VARCHAR(50) PRIMARY KEY,
     streetName VARCHAR(50),
     zipCode VARCHAR(10),
     city VARCHAR(50),
-    telephoneNumber VARCHAR(25),
-    email VARCHAR(50),
-    title VARCHAR(50),
-    hiringDate DATETIME,
-    quit BOOL,
-    FOREIGN KEY (stationId) REFERENCES Station(stationId)
+    country VARCHAR(50)
 );
 
 CREATE TABLE vehicle_category(
@@ -77,16 +33,31 @@ CREATE TABLE vehicle (
   ON UPDATE NO ACTION
 );
 
+CREATE TABLE staff (
+    staffID INT PRIMARY KEY,
+    stationName VARCHAR(50),
+    preName VARCHAR(50),
+    surName VARCHAR(50),
+    streetName VARCHAR(50),
+    zipCode VARCHAR(10),
+    city VARCHAR(50),
+    telephoneNumber VARCHAR(25),
+    email VARCHAR(50),
+    title VARCHAR(50),
+    hiringDate DATETIME,
+    quit BOOL,
+    FOREIGN KEY (stationName) REFERENCES station(stationName)
+);
+
 CREATE TABLE report (
     reportID INT PRIMARY KEY,
     registrationNumber VARCHAR(6),
     staffID INT,
     datum DATETIME,
     descriptionReport VARCHAR(512),
-    FOREIGN KEY (registrationNumber) REFERENCES Fordon(registrationNumber),
-    FOREIGN KEY (staffID) REFERENCES Personal(staffID)
+    FOREIGN KEY (registrationNumber) REFERENCES vehicle(registrationNumber),
+    FOREIGN KEY (staffID) REFERENCES staff(staffID)
 );
-
 
 CREATE TABLE control (
     controlID INT AUTO_INCREMENT PRIMARY KEY,
@@ -95,8 +66,8 @@ CREATE TABLE control (
     datum DATETIME,
     controlLarge BOOL,
     fuelLevel INT,
-    FOREIGN KEY (registrationNumber) REFERENCES Fordon(registrationNumber),
-    FOREIGN KEY (staffID) REFERENCES Personal(staffID)
+    FOREIGN KEY (registrationNumber) REFERENCES vehicle(registrationNumber),
+    FOREIGN KEY (staffID) REFERENCES staff(staffID)
 );
 
 CREATE TABLE damage (
@@ -105,59 +76,7 @@ CREATE TABLE damage (
     fixedDamage BOOL,
     repairedDate DATETIME,
     descriptionDamage VARCHAR(512),
-    FOREIGN KEY (controlID) REFERENCES Kontroll(controlID)
-);
-
-CREATE TABLE station (
-    stationName VARCHAR(50) PRIMARY KEY,
-    streetName VARCHAR(50),
-    zipCode VARCHAR(10),
-    city VARCHAR(50),
-    country VARCHAR(50)
-);
-
-CREATE TABLE booking_details (
-    bookingDetalsID INT AUTO_INCREMENT PRIMARY KEY,
-    bookingNumber INT,
-    registrationNumber VARCHAR(6),
-    FOREIGN KEY (bookingNumber) REFERENCES Bokning(bookingNumber),
-    FOREIGN KEY (registrationNumber) REFERENCES Fordon(registrationNumber)
-);
-
-CREATE TABLE booking (
-    bookingNumber INT AUTO_INCREMENT PRIMARY KEY,
-    stationId INT,
-    orgNumber INT,
-    personalIdentificationNumber VARCHAR(255),
-    offerID INT,
-    startDatum DATETIME,
-    endDatum DATETIME,
-    commentBooking VARCHAR(512),
-    driverLicenseCheck BOOL,
-    cost FLOAT,
-    FOREIGN KEY (stationId) REFERENCES Station(stationId),
-    FOREIGN KEY (orgNumber) REFERENCES ForetagsKund(orgNumber),
-    FOREIGN KEY (personalIdentificationNumber) REFERENCES PrivatKund(personalIdentificationNumber),
-    FOREIGN KEY (offerID) REFERENCES Erbjudande(offerID)
-);
-
-CREATE TABLE contract (
-    contractID INT AUTO_INCREMENT PRIMARY KEY,
-    bookingNumber INT,
-    datum DATETIME,
-    orderingParty VARCHAR(100),
-    FOREIGN KEY (bookingNumber) REFERENCES Bokning(bookingNumber)
-);
-
-CREATE TABLE invoice (
-    invoiceNumber INT PRIMARY KEY,
-    bookingNumber INT,
-    price FLOAT,
-    vatlueAddedTax INT,
-    datum DATETIME,
-    dueDate DATETIME,
-    paid BOOL,
-    FOREIGN KEY (bookingNumber) REFERENCES Bokning(bookingNumber)
+    FOREIGN KEY (controlID) REFERENCES control(controlID)
 );
 
 CREATE TABLE business_customer (
@@ -191,4 +110,48 @@ CREATE TABLE offer (
     endDatum DATETIME,
     freeMileage INT,
     discount INT
+);
+
+CREATE TABLE booking (
+    bookingNumber INT AUTO_INCREMENT PRIMARY KEY,
+    stationName VARCHAR(50),
+    orgNumber INT,
+    personalIdentificationNumber VARCHAR(255),
+    offerID INT,
+    startDatum DATETIME,
+    endDatum DATETIME,
+    commentBooking VARCHAR(512),
+    driverLicenseCheck BOOL,
+    cost FLOAT,
+    FOREIGN KEY (stationName) REFERENCES station(stationName),
+    FOREIGN KEY (orgNumber) REFERENCES business_customer(orgNumber),
+    FOREIGN KEY (personalIdentificationNumber) REFERENCES private_customer(personalIdentificationNumber),
+    FOREIGN KEY (offerID) REFERENCES offer(offerID)
+);
+
+CREATE TABLE booking_details (
+    bookingDetalsID INT AUTO_INCREMENT PRIMARY KEY,
+    bookingNumber INT,
+    registrationNumber VARCHAR(6),
+    FOREIGN KEY (bookingNumber) REFERENCES booking(bookingNumber),
+    FOREIGN KEY (registrationNumber) REFERENCES vehicle(registrationNumber)
+);
+
+CREATE TABLE contract (
+    contractID INT AUTO_INCREMENT PRIMARY KEY,
+    bookingNumber INT,
+    datum DATETIME,
+    orderingParty VARCHAR(100),
+    FOREIGN KEY (bookingNumber) REFERENCES booking(bookingNumber)
+);
+
+CREATE TABLE invoice (
+    invoiceNumber INT PRIMARY KEY,
+    bookingNumber INT,
+    price FLOAT,
+    vatlueAddedTax INT,
+    datum DATETIME,
+    dueDate DATETIME,
+    paid BOOL,
+    FOREIGN KEY (bookingNumber) REFERENCES booking(bookingNumber)
 );
